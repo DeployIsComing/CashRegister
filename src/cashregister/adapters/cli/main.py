e11e95ce73ed6095c51bf24f81e6df8b5254b4d6
Carlos Eduardo da Silva <carlosedasilva@Carloss-MacBook-Pro-2.local>
2026-08-16 20:50:51 -0300

    feat: Added simple test case to test entities

--- src/cashregister/adapters/cli/main.py
+++ src/cashregister/adapters/cli/main.py
@@ -1,12 +1,26 @@
 # Version 0.0.1
 # Cash Register System
 # Author: Carlos Eduardo da Silva <carlosedasilva@gmail.com>
+import time
+import sys
+from cashregister.adapters.cli.simple_entity_tests import test_money_entity
+
 
 def main() -> None:
     """
     Cash register initial flow
     """
-    print("Cash register initial flow test")
+    print("Cash Register [Version 0.0.1]")
+    print("(c) 2026 Carlos Corporation No rights reserved.")
+    print("Connecting to Skynet...")
+    time.sleep(2)
+
+    # system additional options
+    args = sys.argv[1]
+    if args:
+        if args == "--simple-test": # testing entities if true
+            print("Testing Money entity...")
+            test_money_entity()
 
 if __name__ == "__main__":
     main()
---
+++ src/cashregister/adapters/cli/simple_entity_tests.py
@@ -0,0 +1,25 @@
+# Simple testing script to check VOs during development
+from decimal import Decimal
+from cashregister.domain.entities.money import Money
+from cashregister.domain.entities.currency_unit import CurrencyUnit
+
+def test_money_entity() -> None:
+    print("--- Testing Money entity ---")
+
+    # playing with brazilian money
+    m_units = [
+        CurrencyUnit(name="Um Real", value=Decimal("1.00"), plural="real"),
+        CurrencyUnit(name="Cinquenta Reais", value=Decimal("50.00"), plural="reais"),
+        CurrencyUnit(name="Cinquenta Centavos", value=Decimal("0.50"), plural="reais"),
+        CurrencyUnit(name="Vinte Reais", value=Decimal("20.00"), plural="reais"),
+    ]
+
+    money = Money(code="BRL", symbol="R$", currency_unit=m_units)
+
+    print("-----------------------------------------------")
+    print(f"Currency: {money.code} ({money.symbol} = {money.currency_unit})")
+    print("\nCurrency units, should show highest value first:")
+    print("-----------------------------------------------")
+
+    for unit in money.currency_unit:
+        print(f" - {unit.name}: {money.symbol} {unit.value}")
