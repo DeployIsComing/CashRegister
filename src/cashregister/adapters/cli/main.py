1f6b21b9f35b0f32a26855d82ea3a1a6f17d6380
Carlos Eduardo da Silva <carlosedasilva@Carloss-MacBook-Pro-2.local>
2026-08-15 22:15:59 -0300

    (feat): implemented system entry-point

---
+++ src/cashregister/adapters/cli/main.py
@@ -0,0 +1,12 @@
+# Version 0.0.1
+# Cash Register System
+# Author: Carlos Eduardo da Silva <carlosedasilva@gmail.com>
+
+def main() -> None:
+    """
+    Cash register initial flow
+    """
+    print("Cash register initial flow")
+
+if __name__ == "__main__":
+    main()
