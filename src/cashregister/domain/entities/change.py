02195fa72a0b7e42fb18980d217df5556ed44bc1
Carlos Eduardo da Silva <carlosedasilva@Carloss-MacBook-Pro-2.local>
2026-08-16 20:24:27 -0300

    feat: Created the entity change according to specification

--- src/cashregister/domain/entities/change.py
+++ src/cashregister/domain/entities/change.py
@@ -7,6 +7,8 @@ class ChangeLine(BaseModel):
     Output the change the cashier should return to the customer
         1. The return string should look like: 1 dollar,2 quarters,1 nickel, etc ...
         2. Each new line in the input file should be a new line in the output file
+        
+        Example: currency_unit: dollar, quantity: 1
     """
     currency_unit: CurrencyUnit
     quantity: int = Field(gt=0)
