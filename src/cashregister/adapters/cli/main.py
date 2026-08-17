371c2e1db987819b333f379d7ab501641e535bf8
Carlos Eduardo da Silva <carlosedasilva@Carloss-MacBook-Pro-2.local>
2026-08-16 21:16:05 -0300

    feat: organized main.py

--- src/cashregister/adapters/cli/main.py
+++ src/cashregister/adapters/cli/main.py
@@ -12,15 +12,21 @@ def main() -> None:
     """
     print("Cash Register [Version 0.0.1]")
     print("(c) 2026 Carlos Corporation No rights reserved.")
-    print("Connecting to Skynet...")
-    time.sleep(2)
+    time.sleep(1)
+    print("🤖 Connecting to Skynet                   [OK]")
+    time.sleep(1)
+    print("⚛️ Initializing quantum computing         [OK]")
+    time.sleep(1)
+    print("🛰️ Synchronizing starlink constellation   [OK]")
 
     # system additional options
-    args = sys.argv[1]
+    args = sys.argv[1:]
     if args:
         if args == "--simple-test": # testing entities if true
             print("Testing Money entity...")
             test_money_entity()
+
+    # TODO - thinking about in creating an output data for api consumption or mcp
 
 if __name__ == "__main__":
     main()
